825fe19456d2b0069847ab12bd82589d312962e9
Gaurav MIttal <gaurav.mittals100@gmail.com>
2024-06-12 01:37:19 +0530

    thirteenth commit

--- week2/lecture_17/todo-list/src/screens/todoScreens.js
+++ week2/lecture_17/todo-list/src/screens/todoScreens.js
@@ -1,9 +1,27 @@
 import { Component } from "react";
 
 class TodoScreen extends Component{
+    state={
+        taskCount:0,
+    };
     render(){
         return (
-            <h1 className="ui heading center">To DO List</h1>
+            <div className="screen">
+                <h1 className="ui heading center">To DO List</h1>
+                <div>
+                    <button onClick={(e)=>{
+                        this.setState({
+                            ...this.state,
+                            taskCount:this.state.taskCount +1,
+                        });
+                        console.log(`Add Task Button was Clicked ${this.state.taskCount}` );
+                    }} className="ui secondary button"> 
+                        Add Task
+                    </button>
+                </div>
+                <p>No. of Task in the queue {this.state.taskCount}</p>
+            </div>
+
         )
     }
 }
