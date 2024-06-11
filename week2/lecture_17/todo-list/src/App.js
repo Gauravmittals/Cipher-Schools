58b34ec166e4e2a1c118118905fad2bfa13fed2f
Gaurav MIttal <gaurav.mittals100@gmail.com>
2024-06-12 00:07:29 +0530

    twelfth commit

---
+++ week2/lecture_17/todo-list/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import TodoScreen from "./screens/todoScreens";
+class App extends Component{
+  render(){
+    return (
+      <TodoScreen/> 
+
+    )
+  }
+}
+export default App;
---
+++ week2/lecture_17/todo-list/src/screens/todoScreens.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class TodoScreen extends Component{
+    render(){
+        return (
+            <h1 className="ui heading center">To DO List</h1>
+        )
+    }
+}
+
+export default TodoScreen;
