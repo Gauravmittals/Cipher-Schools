905d4712587d49aebdbb95130b71fb4c115ce7eb
Gaurav MIttal <gaurav.mittals100@gmail.com>
2024-07-09 02:09:11 +0530

    Lecture44

---
+++ week5/lecture_42/task-application/src/bcrypt.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcrypt");
+
+const encryptPassword = async (plainTextPassword) => {
+    try {
+        return await bcrypt.hash(plainTextPassword, 8);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+const checkPassword = async (plainTextPassword, hashedPassword) => {
+    try {
+        return await bcrypt.compare(plainTextPassword, hashedPassword);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+module.exports = { checkPassword, encryptPassword };
--- week5/lecture_42/task-application/src/models/User.js
+++ week5/lecture_42/task-application/src/models/User.js
@@ -1,55 +1,7 @@
-// const {model,Schema}= require("mongoose");
-// const {isEmail}=require("validator");
-// const UserSchema =new Schema({
-//     name:{type:String,required:true,trim:true},
-//     email:{type:String,required:true,trim:true,unique:true,lowerCase:true,required:true,validate:{validator(email){
-//         return isEmail(email);
-//     }}},
-//     age:{type:Number, required:true,valitdator:{validate(age){
-//         if(age<0){
-//             throw new Error("Age must be greater than 0");
-//         }
-//         return true;
-
-//     }}},
-//     password:{type:String,required:true,trim:true,minlength:8,validate:{validator(password){
-//         if(password.includes(" ") || password.includes("\n")||password.includes("\t")){
-//             throw new Error("Password cannot contain spaces or tabs");
-//         }
-//         if(password.toLowerCase().includes("password")){
-//             throw new Error("Password cannot contain the word password");
-//         }
-//     },},},
-// },
-// {timestamps:true}
-// );
-// UserSchema.statics.findByEmailAndPasswordForAuth=async(email,password)=>{
-//     try{
-//         const user= await User.findOne({email});
-//         if(!user){
-//             throw new Error("No user with that email");
-//         }
-//         if(password!== user.password){
-//             throw new Error("Incorrect password");
-//         }
-//         console.log("Logged in successfully");
-//         return user;
-
-
-//     }
-//     catch(err){
-//         console.error(err);
-//         throw err;
-//     }
-// }
-// const User=model("User",UserSchema);
-
-// module.exports=User;
-
-
-
 const { model, Schema } = require("mongoose");
 const { isEmail } = require("validator");
+const { encryptPassword, checkPassword } = require("../bcrypt");
+
 
 const UserSchema = new Schema({
     name: { type: String, required: true, trim: true },
@@ -101,7 +53,8 @@ UserSchema.statics.findByEmailAndPasswordForAuth = async (email, password) => {
         if (!user) {
             throw new Error("No user with that email");
         }
-        if (password !== user.password) {
+        const isMatch = checkPassword(password,user.password);
+        if (!isMatch) {
             throw new Error("Incorrect password");
         }
         console.log("Logged in successfully");
@@ -111,6 +64,14 @@ UserSchema.statics.findByEmailAndPasswordForAuth = async (email, password) => {
         throw err;
     }
 };
+
+UserSchema.pre("save",async function(next){
+    const user=this;
+    if(user.modifiedPaths().includes("password")){
+      user.password=await encryptPassword(user.password);
+    }
+next();
+});
 
 const User = model("User", UserSchema);
 
